b6e9950e2c226e46293e0c18e1e48f5217db098e
JesseCoretta <74126726+JesseCoretta@users.noreply.github.com>
2026-06-26 15:20:16 -0700

    resolve conflicting tags for combined/dedicated authorities

--- x660.go
+++ x660.go
@@ -40,9 +40,9 @@ type X660 struct {
 	RC_SAuthyDN []string `ldap:"c-sponsor;collective" json:"c-sponsor,omitempty"`
 
 	// COMBINED registrant entry stubs. See RASCHEMA 2.3.37-53, 2.3.56-73 and 2.3.76-93.
-	R_CFAuthy *FirstAuthority   `json:"firstAuthority"`
-	R_CCAuthy *CurrentAuthority `json:"currentAuthority"`
-	R_CSAuthy *Sponsor          `json:"sponsor"`
+	R_CFAuthy *FirstAuthority   `json:"combinedfirstAuthority"`
+	R_CCAuthy *CurrentAuthority `json:"combinedcurrentAuthority"`
+	R_CSAuthy *Sponsor          `json:"combinedSponsor"`
 
 	r_DITProfile *DITProfile `json:"-"`
 	r_root       *registeredRoot // linked from *Registration during init
